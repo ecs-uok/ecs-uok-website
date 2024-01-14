4791b4a24581e021e11152417b86a0cc9dce1987
Mindula Madhuhansa <76749525+mindula-madhuhansa@users.noreply.github.com>
2024-01-14 09:17:08 +0530

    add blinking red dot

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { doc, getDoc } from "firebase/firestore";
 import { db, storage } from "../config/firebase";
 import { ref, getDownloadURL } from "firebase/storage";
+import RedDot from "./animations/RedDot";
 
 const TeamLogo = ({ imagePath }) => {
   const [imageUrl, setImageUrl] = useState("");
@@ -74,42 +75,55 @@ function Scoreboard() {
     return () => clearInterval(intervalId);
   }, []);
   return (
-    <div className="bg-gray-200 p-4 md:w-1/3 mx-auto md:my-4">
-      <div className="flex flex-col items-center md:flex-row md:justify-around">
-        <h1 className="text-xl font-semibold my-2 uppercase">
-          {firstTeamName} <span className="capitalize">Vs.</span>{" "}
-          {secondTeamName}
-        </h1>
-        <p className="mb-4">{matchData.matchNumber}</p>
-        <div className="flex items-center md:gap-8 gap-24">
-          <div className="flex flex-col items-center">
-            <TeamLogo imagePath={firstTeamName} />
-          </div>
-          <div className="flex flex-col items-center">
-            <p className="font-bold text-xl">
-              {firstTeamScore}/{firstTeamWickets}
-            </p>
-            <p className="font-medium text-base">
-              ({firstTeamBalls}/{matchData.totalBalls})
-            </p>
-          </div>
-        </div>
-
-        <div className="flex items-center md:gap-8 gap-24">
-          <div className="flex flex-col items-center">
-            <TeamLogo imagePath={secondTeamName} />
-          </div>
-          <div className="flex flex-col items-center">
-            <p className="font-bold text-xl">
-              {secondTeamScore}/{secondTeamWickets}
-            </p>
-            <p className="font-medium text-base">
-              ({secondTeamBalls}/{matchData.totalBalls})
-            </p>
+    <>
+      <div className="bg-primary w-full py-6 md:py-12 px-10 md:px-48 transition-all duration-1000">
+        <div className="flex flex-col text-white">
+          <span className="text-white text-2xl md:text-4xl">
+            ECSC Challenge Trophy
+          </span>
+          <div className="flex items-center -ml-2 mt-1">
+            <RedDot />
+            <p className="text-lg ml-2">Live Score</p>
           </div>
         </div>
       </div>
-    </div>
+      <div className="bg-gray-200 p-4 md:w-1/3 mx-auto md:my-4">
+        <div className="flex flex-col items-center md:flex-row md:justify-around">
+          <h1 className="text-xl font-semibold my-2 uppercase">
+            {firstTeamName} <span className="capitalize">Vs. </span>
+            {secondTeamName}
+          </h1>
+          <p className="mb-4">{matchData.matchNumber}</p>
+          <div className="flex items-center md:gap-8 gap-24">
+            <div className="flex flex-col items-center">
+              <TeamLogo imagePath={firstTeamName} />
+            </div>
+            <div className="flex flex-col items-center">
+              <p className="font-bold text-xl">
+                {firstTeamScore}/{firstTeamWickets}
+              </p>
+              <p className="font-medium text-base">
+                ({firstTeamBalls}/{matchData.totalBalls})
+              </p>
+            </div>
+          </div>
+
+          <div className="flex items-center md:gap-8 gap-24">
+            <div className="flex flex-col items-center">
+              <TeamLogo imagePath={secondTeamName} />
+            </div>
+            <div className="flex flex-col items-center">
+              <p className="font-bold text-xl">
+                {secondTeamScore}/{secondTeamWickets}
+              </p>
+              <p className="font-medium text-base">
+                ({secondTeamBalls}/{matchData.totalBalls})
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
