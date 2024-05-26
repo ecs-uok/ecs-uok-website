df59f5abb2856acd2e7d0b666111ec0e3326defc
mindula-madhuhansa <mimmindula@gmail.com>
2024-05-26 19:21:14 +0530

    add fix

--- src/pages/EWasteWeekPage.jsx
+++ src/pages/EWasteWeekPage.jsx
@@ -4,7 +4,7 @@ import EWasteLogo from "../assets/ewasteLogo.jpeg";
 
 function EWasteWeekPage() {
   return (
-    <div className="pt-20 h-screen w-full">
+    <div className="py-20">
       <h1 className="mt-12 text-center text-5xl md:text-7xl font-bold">
         E-Waste Collection
       </h1>
@@ -15,7 +15,7 @@ function EWasteWeekPage() {
       <div className="flex flex-col items-center mt-8">
         <img
           src={EWasteBanner}
-          alt="E Waste Banner"
+          alt="EWaste Banner"
           width={512}
           height={512}
           className=" md:w-[640px]  md:h-[640px]"
@@ -23,14 +23,14 @@ function EWasteWeekPage() {
         <div className="flex flex-row items-center gap-x-8">
           <img
             src={ECSCLogo}
-            alt="E Waste Banner"
+            alt="ECSC Logo"
             width={128}
             height={128}
             className="w-24 h-16 md:w-24 md:h-24 object-contain"
           />
           <img
             src={EWasteLogo}
-            alt="E Waste Banner"
+            alt="EWaste Logo"
             width={256}
             height={128}
             className="w-32 h-16 md:w-48 md:h-32 object-contain"
