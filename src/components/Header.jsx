99ae297fcc27595f2a862c7851e1b7541d9872e7
Binath Perera <binathperera@bluelotus360.co>
2024-06-03 10:39:49 +0530

    make navigation changes for robot battle site

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -96,7 +96,7 @@ export default function Header() {
                   : `hidden`
               }
             >
-              <Link to={"https://robotbattles.ecsc-uok.com/"} target="_blank">
+              <Link to={"/robot_battles"} target="_blank">
                 <li className="py-6" onClick={handleCloseMenu2}>
                   UOK Robot Battles
                 </li>
@@ -184,7 +184,7 @@ export default function Header() {
         </li>
         {toggleMenu2 ? (
           <div className="pl-4">
-            <Link to={"https://robotbattles.ecsc-uok.com/"} target="_blank">
+            <Link to={"/robot_battles"} target="_blank">
               <li
                 className="p-4"
                 onClick={() => {
--- src/components/RoboBattleBanner.jsx
+++ src/components/RoboBattleBanner.jsx
@@ -45,6 +45,15 @@ function RoboBattleBanner() {
                 RULES & REGULATIONS
               </button>
             </a>
+            <a
+              href="https://robotbattles.ecsc-uok.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <button className="px-8 py-2 w-72 h-12 md:text-lg rounded-lg hover:bg-secondary hover:text-white font-semibold border-none">
+                Visit Site
+              </button>
+            </a>
           </div>
         </div>
         <img
