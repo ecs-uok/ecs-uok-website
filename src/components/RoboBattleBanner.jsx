f695c04637862a3034407c8c58a2db5fc4d8876b
mindula-madhuhansa <mimmindula@gmail.com>
2024-07-30 13:21:04 +0530

    add fix

--- src/components/RoboBattleBanner.jsx
+++ src/components/RoboBattleBanner.jsx
@@ -33,7 +33,7 @@ function RoboBattleBanner() {
               target="_blank"
               rel="noreferrer"
             >
-              <button className="uppercase px-8 py-2 w-72 h-12 md:text-lg rounded-lg hover:bg-secondary hover:text-white font-semibold border-none opacity-50">
+              <button className="uppercase px-8 py-2 w-72 h-12 md:text-lg rounded-lg hover:bg-secondary hover:text-white font-semibold border-none">
                 Register
               </button>
             </a>
