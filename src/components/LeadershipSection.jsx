fd625b23461a5713e5ea96a17866eefa8388cc0d
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2024-08-10 17:03:36 +0530

    try to solve the order issue

--- src/components/LeadershipSection.jsx
+++ src/components/LeadershipSection.jsx
@@ -28,7 +28,7 @@ export default function LeadershipSection() {
       ) : loading ? (
         <Loading />
       ) : (
-        <div className="grid grid-cols-1 gap-4 m-4 sm:grid-cols-2 lg:grid-cols-3 md:gap-8 md:m-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8 m-4 md:m-8">
   {cachedData &&
     cachedData.data
       .sort((a, b) => a.attributes.memberId - b.attributes.memberId) // Sort by memberId
@@ -41,7 +41,7 @@ export default function LeadershipSection() {
           member_role={member.attributes.memberRole}
           member_email={member.attributes.memberEmail}
         />
-      ))}
+      ))}
 </div>
       )}
     </div>
